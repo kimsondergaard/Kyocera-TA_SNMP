2f498b1fad9477d275eea57a6f63480bb30c766a
kimsondergaard <kim@kimsondergaard.dk>
2017-05-18 12:05:06 +0200

    Update snmp.sh

--- snmp.sh
+++ snmp.sh
@@ -68,7 +68,7 @@ yellow=${yresult%.*}
 
 #WRITE TO MYSQL
 NOW=$(date +"%d/%m-%Y")
-TS=date +%s
+TS=$(date +"%s")
 
 mysql --host=${host} --user=${user} --password=${pw}  ${db}  -e "INSERT INTO ${tb}  (SN,MN,IP,HN,scan,total,A3C,A3M,A4C,A4M,c,m,y,k,dato,TS)
  VALUES('${serial}','${model}','${ip}','${hostname}','${scan}','${total}','${a3c}','${a3m}','${a4c}','${a4m}','${cyan}','${magenta}','${yellow}','${key}','${NOW}','${TS}');"
